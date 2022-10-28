451cff4305a82ac4846ee29c6d26a53ecc3365f4
emiragovic <eagovic1@etf.unsa.ba>
2022-10-28 04:00:26 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner unos = new Scanner(System.in);
+        ArrayList<Double> L = new ArrayList<Double>();
+        while (true) {
+            System.out.println("Unesite broj: ");
+            if (unos.hasNextDouble()) {
+                L.add(unos.nextDouble());
+                continue;
+            }
+
+            if (unos.next().equalsIgnoreCase("stop"))
+                break;
+        }
+
+        double standardnaDevijacija = 0, srednjaVrijednost = 0;
+
+        for (Double el : L)
+                srednjaVrijednost += el;
+        srednjaVrijednost /= L.size();
+
+        for (Double el : L)
+            standardnaDevijacija += Math.pow((el - srednjaVrijednost),2);
+        standardnaDevijacija /= L.size();
+        standardnaDevijacija = Math.sqrt(standardnaDevijacija);
+
+
+        System.out.println("Elementi liste (uneseni brojevi): ");
+        for (Double el : L)
+            System.out.print(el + " ");
+        System.out.println("\nMaksimalni element liste: " + Collections.max(L));
+        System.out.println("Minimalni element liste: " + Collections.min(L));
+        System.out.println("Srednja vrijednost elemenata liste: " + srednjaVrijednost);
+        System.out.println("Standardna devijacija elemenata liste: " + standardnaDevijacija);
+    }
+}
